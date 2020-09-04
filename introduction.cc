14100af94222582d42bdc43d03c08f78e57e1f67
Ethan Johnson <61812828+wheattthins@users.noreply.github.com>
2020-09-04 15:57:02 -0500

    Create introduction.cc

---
+++ introduction.cc
@@ -0,0 +1,22 @@
+/* 	Ethan Johnson
+	z1826490
+	CSCI 340 Section 2
+	
+	I certify that this is my own work and where appropriate an extension 
+	of the starter code provided for the assignment.
+*/
+#include <iostream>
+
+using namespace std;
+
+int
+main(int argc, char *argv[])
+{
+
+	cut << "Data Structures are Important!!" << endl;   // Yes, I know there is a typo!!!
+	
+	cout << "I'm very excited to be taking Data Structures" << endl;
+	
+	return 0;
+
+}// End of main() routine.
